cda29035b93a09840a3801c09ca3c2ea03c30ad4
David <david.boots@gmail.com>
2017-07-29 23:01:20 -0500

    Begin implementing CommNetManager so that active beacons have no signal degradation between them.

---
+++ Source/BeaconCommNet/BeaconCommNetScenario.cs
@@ -0,0 +1,47 @@
+﻿using CommNet;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeaconCommNet.CommNetLayer
+{
+    /// <summary>
+    /// This class is the key that allows to break into and customise KSP's CommNet. This is possibly the secondary model in the Model–view–controller sense
+    /// </summary>
+    [KSPScenario(ScenarioCreationOptions.AddToAllGames, new GameScenes[] {GameScenes.FLIGHT, GameScenes.TRACKSTATION, GameScenes.EDITOR })]
+    public class BeaconCommNetScenario : CommNetScenario
+    {
+        /* Note:
+         * 1) On entering a desired scene, OnLoad() and then Start() are called.
+         * 2) On leaving the scene, OnSave() is called
+         * 3) GameScenes.SPACECENTER is recommended so that the constellation data can be verified and error-corrected in advance
+         */
+        
+        internal CommNetNetwork CustomCommNetNetwork = null;
+
+        public static new BeaconCommNetScenario Instance
+        {
+            get;
+            protected set;
+        }
+
+        protected override void Start()
+        {
+            BeaconCommNetScenario.Instance = this;
+
+            //Replace the CommNet network
+            // Use CommNetManager's methods:
+            CommNetManagerAPI.CommNetManagerChecker.SetCommNetManagerIfAvailable(this, typeof(BeaconCommNetNetwork), out CustomCommNetNetwork);
+        }
+
+        public override void OnAwake()
+        {
+            //override to turn off CommNetScenario's instance check
+        }
+
+        private void OnDestroy()
+        {
+            if (this.CustomCommNetNetwork != null)
+                UnityEngine.Object.Destroy(this.CustomCommNetNetwork);
+        }
+    }
+}
